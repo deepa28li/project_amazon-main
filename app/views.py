d740f6ed54f36dfa60984ad930c793d1f3d4801c
deepa28li <swaralipatil2000@gmail.com>
2024-02-27 18:29:53 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -4,8 +4,9 @@ from django.shortcuts import render
 from app.forms import *
 from django.http import HttpResponse,HttpResponseRedirect
 from django.core.mail import send_mail
-from django.contrib.auth import authenticate,login
-
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 
 
 # ----------registration-------------
@@ -26,7 +27,7 @@ def registration(request):
             
             send_mail('registartion',
                       'registration is sucessful in amazon ',
-                      'oppalanaveen143@gmail.com',
+                      'deepalipatil2800@gmail.com',
                       [NSUFO.email],
                       fail_silently=False )
             return HttpResponse('registration form is successful check in admin')        
@@ -43,3 +44,43 @@ def home(request):
         return render(request,'home.html',d)
     return render(request,'home.html')
 
+# --------------login page------------------
+
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        AUO=authenticate(username=username,password=password)
+        if AUO:
+            if AUO.is_active:
+                login(request,AUO)
+                request.session['username']=username
+                return HttpResponseRedirect(reverse('home'))
+            else:
+                return HttpResponse('Not a Active User')
+        else:
+            return HttpResponse('Invalid Details')
+    return render(request,'user_login.html')
+
+# ---------------reset password ----------------
+
+def reset_password(request):
+    if request.method=='POST':
+        password=request.POST['pw']
+        username=request.POST['un']
+        LUO=User.objects.filter(username=username)
+        if LUO:
+            UO=LUO[0]
+            UO.set_password(password)
+            UO.save()
+            return HttpResponse('reset_password is done')
+        else:
+            return HttpResponse('invalid data')
+    return render(request,'reset_password.html')
+
+# -------------logout------------------
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
--- project_amazon/urls.py
+++ project_amazon/urls.py
@@ -17,8 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from app.views import *
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('registration/',registration,name='registration'),
     path('home/',home,name='home'),
-]
+    path('user_login/',user_login,name='user_login'),
+    path('reset_password/',reset_password,name='reset_password'),
+    path('user_logout/',user_logout,name='user_logout'),
+
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
